1012c89e33ef0a2ca33f5bf3aeb5b1b1b885df96
LuisNavarroMontes <luis.navarro.montes@gmail.com>
2024-01-25 20:14:10 +0100

    main

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     }
   });
   // Iniciar el servidor
-  const PORT = process.env.PORT;
+  const PORT = 4000;
   server.listen(PORT, () => {
     console.log(`Servidor Socket.IO escuchando en el puerto ${PORT}`);
   });
