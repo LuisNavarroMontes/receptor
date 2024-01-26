948f871f56bd673e7d6d87626b84c1f127fdec95
LuisNavarroMontes <luis.navarro.montes@gmail.com>
2024-01-26 01:53:48 +0100

    main

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   // Crear el servidor TCP
   const tcpServer = net.createServer((socket) => {
      socket.on('data', (data) => {
