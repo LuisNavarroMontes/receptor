ae4f2e82ca36f2ac43d800e800af526789c816e0
LuisNavarroMontes <luis.navarro.montes@gmail.com>
2024-02-10 22:50:02 +0100

    persistencia

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,28 @@ import * as express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 
+interface Device {
+  id: number;
+  nombre: string;
+  estado: string;
+  temperatura: number[];
+}
+
+interface JSONData {
+  N_con: number;
+  [key: string]: Device | number;
+}
+
+
 async function bootstrap() {
     const app = express();
     const httpServer = createServer(app);
     await httpServer.listen(process.env.PORT || 3000);
     console.log('Socket.IO server listening on port', process.env.PORT || 3000);
 
-    let json = {
-        "N_con": 0,
+    let json: JSONData = {
+      "N_con": 0,
     };
-
     const io = new Server(httpServer, {
         cors: {
             origin: "*",
@@ -24,27 +36,32 @@ async function bootstrap() {
         console.log('User connected to the socket');
 
         socket.on('data', (data) => {
-            const newDevices = Object.keys(data).filter(key => key.startsWith('IoT'));
-            newDevices.forEach(deviceKey => {
-                const deviceData = data[deviceKey];
-                let existingDevice = json[deviceKey];
-                if (existingDevice) {
-                    // Si el dispositivo ya existe, agregamos la temperatura al array existente
-                    existingDevice.temperatura.push(deviceData.temperatura);
-                } else {
-                    // Si el dispositivo no existe, lo agregamos al JSON
-                    json[deviceKey] = {
-                        ...deviceData,
-                        "temperatura": [deviceData.temperatura]
-                    };
-                }
-            });
-
-            // Actualizamos la cantidad de dispositivos IoT
-            json.N_con = Object.keys(json).filter(key => key.startsWith('IoT')).length;
-            console.log('Modified JSON:', json);
-            socket.broadcast.emit('message', json);
-        });
+          const newDevices = Object.keys(data).filter(key => key.startsWith('IoT'));
+          newDevices.forEach(deviceKey => {
+              const deviceData = data[deviceKey];
+              let existingDevice = Object.values(json).find(device => {
+                  return typeof device !== 'number' && device.id === deviceData.id;
+              });
+              if (existingDevice) {
+                  if (typeof existingDevice !== 'number') {
+                      // Si el dispositivo ya existe, agregamos la temperatura al array existente
+                      existingDevice.temperatura.push(deviceData.temperatura);
+                  }
+              } else {
+                  // Si el dispositivo no existe, lo agregamos al JSON
+                  json[deviceKey] = {
+                      ...deviceData,
+                      "id": deviceData.id,
+                      "temperatura": [deviceData.temperatura]
+                  };
+              }
+          });
+      
+          // Actualizamos la cantidad de dispositivos IoT
+          json.N_con = Object.keys(json).filter(key => key.startsWith('IoT')).length;
+          console.log('Modified JSON:', json);
+          socket.broadcast.emit('message', json);
+      });
 
         socket.on('disconnect', () => {
             console.log('User disconnected');
